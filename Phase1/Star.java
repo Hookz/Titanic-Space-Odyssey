5b69d89f6c63711c3721b14afe096c96dd7c974b
Th0github <43998700+Th0github@users.noreply.github.com>
2019-03-18 11:47:35 +0100

    Add files via upload

---
+++ Phase1/Star.java
@@ -0,0 +1,16 @@
+package SolarSystem;
+
+import java.awt.Color;
+import java.awt.geom.Path2D;
+
+public class Star extends Mision
+{
+    public Color color;
+
+    public Star(String name,Color c, double posX,double posY,double mass,double radius,double speedX,double speedY,double rotation)
+    {
+        super(name,c,posX,posY,mass,radius,speedX,speedY,rotation);
+        path = new Path2D.Double();
+        path.moveTo(posX, -posY);
+    }
+}
