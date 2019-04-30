50fc0fec2b04c95ffb71e89c39f76d9f85d63584
nelisneel1 <48676836+nelisneel1@users.noreply.github.com>
2019-04-30 09:54:50 +0200

    Add SpaceShip class

---
+++ Phase 2/SpaceShip.java
@@ -0,0 +1,85 @@
+public class SpaceShip {
+
+    private double mass; //kg
+    private double xVelocity;//
+    private double yVelocity;//
+    private double xLocation;//km
+    private double yLocation;//km
+    private double gravity;//acceleration of gravity
+    private static final double massTitan = 1.3452E+23; //kg
+    private static final double G = 6.67E-11;
+    private double tilt; //radians
+    private double angularVelocity; // rad/s
+    private static final double spinTolerance = 0.02; //radians/s
+    private static final double tiltTolerance = 0.01; //rad
+    private static final double landingXTolerance = 0.01; //m/s
+    //TODO add ytolerance
+
+
+    public SpaceShip(double mass, double xLocation, double yLocation) {
+        this.mass = mass;
+        this.yLocation=yLocation;
+        this.xLocation=xLocation;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setXLocation(double xLocation) {
+        this.xLocation = xLocation;
+    }
+
+    public double getXLocation() {
+        return xLocation;
+    }
+
+    public void setYLocation(double yLocation) {
+        this.yLocation = yLocation;
+    }
+
+    public double getYLocation() {
+        return yLocation;
+    }
+
+    public void setXVelocity(double xVelocity){
+        this.xVelocity = xVelocity;
+    }
+
+    public double getXVelocity(){
+        return xVelocity;
+    }
+
+    public void setYVelocity(double yVelocity){
+        this.yVelocity = yVelocity;
+    }
+
+    public double getYVelocity(){
+        return yVelocity;
+    }
+
+    public void setTilt(double tilt) {
+        this.tilt = tilt;
+    }
+
+    public double getTilt() {
+        return tilt;
+    }
+
+    public double getGravity() {
+        double metersToSurface = getYLocation() * 1000;
+        gravity = (getMass()*massTitan*G)/(metersToSurface*metersToSurface);
+        return gravity;
+    }
+
+    public static void main (String[] args) {
+    SpaceShip s = new SpaceShip(500,100,100);
+    System.out.println(s.getGravity());
+    }
+
+    }
+
