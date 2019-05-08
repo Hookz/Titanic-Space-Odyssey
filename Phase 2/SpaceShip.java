f4e48165a9b5856a9881ee92dce7dac65568dd40
Niels van der Pas <nielsvanderpas15@gmail.com>
2019-05-08 15:54:31 +0200

    Add formulae for acceleration

--- Phase 2/SpaceShip.java
+++ Phase 2/SpaceShip.java
@@ -7,12 +7,17 @@ public class SpaceShip {
     private double yLocation;//km
     private double gravity;//acceleration of gravity
     private static final double massTitan = 1.3452E+23; //kg
+    public static final double gravTitan = 1.352; //acceleration due to gravity on titan, in ms
     private static final double G = 6.67E-11;
     private double tilt; //radians
     private double angularVelocity; // rad/s
     private static final double spinTolerance = 0.02; //radians/s
     private static final double tiltTolerance = 0.01; //rad
     private static final double landingXTolerance = 0.01; //m/s
+    private double xAcceleration;
+    private double yAcceleration;
+    private double torque; //provided by the side thrusters
+    private double rotationAcceleration;
     //TODO add ytolerance
 
 
@@ -76,6 +81,23 @@ public class SpaceShip {
         return gravity;
     }
 
-
+    //These are formulae for the acceleration
+    public double calculateXAcceleration(double rotationInRads, double accelerationThruster){
+        this.xAcceleration = accelerationThruster*Math.sin(rotationInRads);
+        return xAcceleration;
     }
+
+    public double calculateYAcceleration(double rotationInRads, double accelerationThruster){
+        this.yAcceleration = accelerationThruster*Math.cos(rotationInRads)-gravTitan;
+        return yAcceleration;
+    }
+
+    public double calculateRotationAcceleration(){
+        this.rotationAcceleration = torque;
+        return rotationAcceleration;
+    }
+    //By choosing realistic values for the power of the thrusters we can now calculate the acceleration
+
+
+}
 
