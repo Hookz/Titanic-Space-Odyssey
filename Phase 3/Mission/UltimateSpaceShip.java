fce4bb4a08a4b6ebfd17ed05755439491b207bf6
Janneke 13 <j.vanbaden@ziggo.nl>
2019-06-12 13:57:07 +0200

    added if conditions

--- Phase 3/Mission/UltimateSpaceShip.java
+++ Phase 3/Mission/UltimateSpaceShip.java
@@ -1,5 +1,45 @@
 package Mission;
 
-public class UltimateSpaceShip {
 //the only spaceship class we'll need
+
+public class UltimateSpaceShip {
+	
+	private boolean goingToTitan = true;
+	private boolean landingOnTitan = false;
+	private boolean goingToEarth = false;
+	private boolean landingOnEarth = false;
+	
+	public void update(int timeSlice) {
+		if(goingToTitan) {
+			
+			if() {
+				goingToTitan = false;
+				landingOnTitan = true;
+			}
+		}
+		else if(landingOnTitan) {
+			if() {
+				landingOnTitan = false;
+				goingToEarth = true;
+			}
+		}
+		else if(goingToEarth) {
+			if() {
+				goingToEarth = false;
+				landingOnEarth = true;
+			}
+			
+		}
+		else if(landingOnEarth) {
+			if(){
+				landingOnEarth = false;
+			}
+		}
+		else {
+			System.out.println("No more updates");
+		}
+		
+	}
+	
+
 }
