e433216d68e28b3dbcafca04fd68fd9d99f087de
Adrien <adrienigcse0003@gmail.com>
2019-06-14 15:33:48 +0200

    added the rotational period of titan to find geostationary orbit

--- GUIDDD/Data/Physics.java
+++ GUIDDD/Data/Physics.java
@@ -5,6 +5,6 @@ public final class Physics {
 
     /** Universal gravity constant */
     public static final double G = 6.67300E-11;
-
+    public static final double T_OF_TITAN = 1377648; //rotational period of titan in seconds
 }
 
