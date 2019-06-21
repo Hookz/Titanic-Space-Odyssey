a1d19d684f09d6e68a4674c5fa0493f3ba443c81
Th0github <tdtsijpkens@gmail.com>
2019-06-21 19:45:30 +0200

    ClosedLoop now stores coordinate in arraylist

--- Phase 2/ControlSystem/ClosedLoopV2.java
+++ Phase 2/ControlSystem/ClosedLoopV2.java
@@ -1,28 +1,24 @@
 package ControlSystem;
 
-import FinalDifferentialEq.RungeKutta;
-
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class ClosedLoopV2 {
-    private static ArrayList<Vector3D> velocityList;
-//    private static ArrayList<Vector3D> coordinatesList;
 
     public static ArrayList<Vector3D> storeCoordinate() {
         ArrayList<Vector3D> coordinatesList = new ArrayList<>();
         Vector2D thruster = new Vector2D(0,0); //x is u, y is v
 
         double timePassed = 0.1;
-        double t = 0;
 
-        while(spaceShip.coordinates.y > 0) {
+
+        while(timePassed < 10000) {
             //temp for old velocity and location
             double tempT;
             Vector3D tempLocation = spaceShip.coordinates;
             Vector3D tempVelocity = velocity;
 
-            //tempLocation = spaceShip.coordinates;
-            //tempVelocity = velocity;
+
 
             spaceShip.coordinates.y = (spaceShip.coordinates.y - spaceShip.coordinates.y * timePassed);
             spaceShip.coordinates.z = (spaceShip.calcTilt(spaceShip.coordinates.y));
@@ -33,58 +29,43 @@ public class ClosedLoopV2 {
             double newVZ = (tempLocation.z + spaceShip.calcTilt(spaceShip.coordinates.y)) /timePassed;
 
             //change velocity
-            velocity.x = newVX; //change time-step for dynamic solver :)
+            velocity.x = newVX;
             velocity.y = newVY;
             velocity.z = newVZ;
 
-//            System.out.println("T velocity x: " + velocity.x);
-            double tx = 2 * (tempLocation.x) / velocity.x; //use this as time
-//            System.out.println("T t x: " + tx);
+            double tx = 2 * (tempLocation.x) / velocity.x;
             double a = velocity.x * velocity.z / (2 * tempLocation.x);
-//            System.out.println("T a x: " + a);
             velocity.x = (velocity.x + a * tx);
-//            System.out.println("T new velocity x: " + velocity.x);
             thurster.x = (velocity.x * velocity.x / (2 * tempLocation.x * Math.sin(spaceShip.coordinates.z)));
-//            System.out.println("T thruster x: " + thurster.x); //print this out
             spaceShip.coordinates.x =(thurster.x * Math.sin(spaceShip.coordinates.z));
-//            System.out.println("New x: " + spaceShip.coordinates.x);
 
-//            System.out.println("T velocity z: " + velocity.z);
-//            System.out.println("T z: " + spaceShip.coordinates.z);
+
             double vDot = rungeKutta4rth(timePassed, velocity.z, 1, spaceShip.TIME_SLICE);
-//            System.out.println("vDot: " + vDot);
             double ty = Math.sqrt((spaceShip.coordinates.z - tempLocation.z) / vDot);
-//            System.out.println("T t z: " + ty);
             velocity.z = (tempVelocity.z + velocity.z * ty);
-//            System.out.println("T velocity z: " + velocity.z);
             spaceShip.coordinates.z = (spaceShip.coordinates.z + tempVelocity.z * ty + vDot*ty*ty /2);
-//            System.out.println("T new z: " + spaceShip.coordinates.z);
 
-            tempT = ty + 0.1 + tx;
-            t += tempT;
 
             coordinatesList.add(spaceShip.coordinates);
 
             timePassed += spaceShip.TIME_SLICE;
-            if(spaceShip.coordinates.x > 0.1 || spaceShip.coordinates.x < -0.1) {
-                continue;
+            if(spaceShip.coordinates.x <= 0.1 && spaceShip.coordinates.x >= -0.1) {
+                coordinatesList.add(spaceShip.coordinates);
+                //fully rotated
+                thurster.x = (g / Math.cos(spaceShip.coordinates.z));
+                spaceShip.coordinates.y = (thurster.x * Math.cos(spaceShip.coordinates.z) - g);
+                timePassed += 0.1;
+
             }  else {
 
-                coordinatesList.add(spaceShip.coordinates);
-//                System.out.println("T velocity y: " + velocity.y);
-                //fully rotated
-//                System.out.println("T coordinate y: " + spaceShip.coordinates.z);
-                thurster.x = (g / Math.cos(spaceShip.coordinates.z));
-//                System.out.println("T thruster y: " + thurster.x);
-                spaceShip.coordinates.y = (thurster.x * Math.cos(spaceShip.coordinates.z) - g);
-//                System.out.println("T new y: " + spaceShip.coordinates.y);
-
-                t += tempT;
+                timePassed+=0.1;
+                continue;
             }
+
+
 
             timePassed += spaceShip.TIME_SLICE;
         }
-
         System.out.println("Total time: " + timePassed);
         return coordinatesList;
     }
@@ -93,7 +74,7 @@ public class ClosedLoopV2 {
     public static Vector2D thurster = new Vector2D();
     public static final double g = 1.352;
 
-    public static SpaceShip spaceShip = new SpaceShip(5000,200,200,0,600,600);
+    public static SpaceShip spaceShip = new SpaceShip(5000,200,200,0.0,600,600);
 
     public static double yDisplacement(double y0) {
         return y0/4;
@@ -152,11 +133,11 @@ public class ClosedLoopV2 {
     public static void main(String args[]) {
         ArrayList<Vector3D> s = storeCoordinate();
         Vector3D temp = null;
-        for(int i = 0; i<s.size();i++){
+        for(int i = 0; i<s.size(); i++){
             temp = s.get(i);
-            System.out.println("x = " + temp.x + " at " + i);
-            System.out.println("y = " + temp.y + " at " + i);
-            System.out.println("z = " + temp.z + " at " + i);
+            System.out.println("x = " + temp.x + " at index " + i);
+            System.out.println("y = " + temp.y + " at index " + i);
+            System.out.println("z = " + temp.z + " at index " + i);
         }
     }
 }
